ac7eeaf00eb327773dd8268d8877df309238ee92
berdiyorova <rano.baxromovna@gmail.com>
2024-10-24 19:48:27 +0500

    get_colors() function added to ProductListView and created orders app

--- products/views.py
+++ products/views.py
@@ -9,20 +9,34 @@ class ProductListView(ListView):
     model = ProductModel
     context_object_name = 'products'
 
+    @staticmethod
+    def get_colors():
+        colors = ColorModel.objects.all()
+        new_colors = list()
+        temp_color = list()
+
+        for color in colors:
+            if len(temp_color) == 2:
+                new_colors.append(temp_color)
+                temp_color.clear()
+            else:
+                temp_color.append(color)
+
+        if temp_color:
+            new_colors.append(temp_color)
+
+        return new_colors
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        parents = context['parents'] = CategoryModel.objects.filter(parent=None)
 
-        categories = list()
-        for parent in parents:
-            for child in parent.children.all():
-                categories.append(child)
-        context['categories'] = categories[:5]
-
+        context['parents'] = CategoryModel.objects.filter(parent=None)
+        context['categories'] = CategoryModel.objects.exclude(parent=None)
         context['category'] = get_object_or_404(CategoryModel, id=self.kwargs['category_id'])
+
         context['tags'] = TagModel.objects.all()
         context['brands'] = BrandModel.objects.all()
-        context['colors'] = ColorModel.objects.all()
+        context['colors'] = self.get_colors()
         context['sizes'] = SizeModel.objects.all()
         return context
 
@@ -31,11 +45,25 @@ class ProductListView(ListView):
         category_id = self.kwargs['category_id']
         category = CategoryModel.objects.filter(id=category_id).first()
 
-        if category.parent is None:
-            categories = category.children.all()
-            products = list()
-            for cat in categories:
-                products += ProductModel.objects.filter(categories=cat)
-            return products
+        order = self.request.GET.get('order')
+        products = list()
 
-        return ProductModel.objects.filter(categories=category)
+        if category.parent is None:
+            for cat in category.children.all():
+                products += ProductModel.objects.filter(categories=cat)
+        else:
+            products = ProductModel.objects.filter(categories=category)
+
+        if order == 'name_asc':
+            products = products.order_by('name')
+        elif order == 'name_desc':
+            products = products.order_by('-name')
+        elif order == 'price_asc':
+            products = products.order_by('real_price')
+        elif order == 'price_desc':
+            products = products.order_by('-real_price')
+        else:
+            products = products.order_by('-created_at')
+
+        return products
+
